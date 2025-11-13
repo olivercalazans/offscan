d0864b865e609bc6fcbe30d7c729407478260e09
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-11-13 00:15:13 -0300

    refac flood for better performance

--- src/engines/flood.rs
+++ src/engines/flood.rs
@@ -8,34 +8,10 @@ use crate::utils::inline_display;
 
 
 
-struct PacketInfo {
-    src_mac:  [u8; 6],
-    src_ip:   Ipv4Addr,
-    src_port: u16,
-    dst_mac:  [u8; 6],
-    dst_ip:   Ipv4Addr,
-}
-
-
-impl PacketInfo {
-    fn new() -> Self {
-        Self {
-            src_mac:  [0; 6],
-            src_ip:   Ipv4Addr::new(0, 0, 0, 0),
-            src_port: 0,
-            dst_mac:  [0; 6],
-            dst_ip:   Ipv4Addr::new(0, 0, 0, 0)
-        }
-    }
-}
-
-
-
 pub struct PacketFlooder {
     args:      FloodArgs,
     start:     u32,
     end:       u32,
-    pkt_info:  PacketInfo,
     pkts_sent: usize,
     rng:       RandValues,
 }
@@ -49,7 +25,6 @@ impl PacketFlooder {
             args,
             start:     0,
             end:       0,
-            pkt_info:  PacketInfo::new(),
             pkts_sent: 0,
             rng:       RandValues::new(),
         }
@@ -77,7 +52,7 @@ impl PacketFlooder {
 
 
     fn set_proto_flags(&mut self) {
-        if self.args.tcp && self.args.udp && self.args.icmp {
+        if !self.args.tcp && !self.args.udp && !self.args.icmp {
             self.args.tcp  = true;
             self.args.udp  = true; 
             self.args.icmp = true;
@@ -86,16 +61,18 @@ impl PacketFlooder {
 
 
 
-    fn setup_tools(iface: &str) -> (PacketBuilder, Layer2RawSocket) {
-        let pkt_builder = PacketBuilder::new();
-        let pkt_sender  = Layer2RawSocket::new(&iface);
-        (pkt_builder, pkt_sender)
+    fn setup_tools(iface: &str) -> PktTools {
+        PktTools {
+            info:    PacketInfo::new(),
+            builder: PacketBuilder::new(),
+            socket:  Layer2RawSocket::new(&iface),
+        }
     }
 
 
 
     fn send_endlessly(&mut self) {
-        let (mut pkt_builder, socket) = Self::setup_tools(&self.args.iface);
+        let mut tools = Self::setup_tools(&self.args.iface);
 
         let fixed_src_ip  = self.args.src_ip;
         let fixed_src_mac = self.args.src_mac;
@@ -103,51 +80,99 @@ impl PacketFlooder {
         let fixed_dst_mac = self.args.dst_mac;
 
         loop {
-            self.pkt_info.src_port = self.rng.get_random_port();
-            self.pkt_info.src_ip   = fixed_src_ip.unwrap_or_else( || self.rng.get_random_ip(self.start, self.end));
-            self.pkt_info.src_mac  = fixed_src_mac.unwrap_or_else(|| self.rng.get_random_mac());
-            self.pkt_info.dst_ip   = fixed_dst_ip.unwrap_or_else( || self.rng.get_random_ip(self.start, self.end));
-            self.pkt_info.dst_mac  = fixed_dst_mac.unwrap_or_else(|| self.rng.get_random_mac());
+            tools.info.src_port = self.rng.get_random_port();
+            tools.info.src_ip   = fixed_src_ip.unwrap_or_else( || self.rng.get_random_ip(self.start, self.end));
+            tools.info.src_mac  = fixed_src_mac.unwrap_or_else(|| self.rng.get_random_mac());
+            tools.info.dst_ip   = fixed_dst_ip.unwrap_or_else( || self.rng.get_random_ip(self.start, self.end));
+            tools.info.dst_mac  = fixed_dst_mac.unwrap_or_else(|| self.rng.get_random_mac());
             
-            self.send_packets(&mut pkt_builder, &socket);
-            self.display_progress();
+            self.send_packets(&mut tools);
+            inline_display(format!("Packets sent: {}", &self.pkts_sent));
         }
     }
 
 
 
-    fn send_packets(&self, pkt_builder: &mut PacketBuilder, socket: &Layer2RawSocket) {
+    #[inline]
+    fn send_packets(&mut self, tools: &mut PktTools) {
         if self.args.tcp {
-            let tcp_pkt = pkt_builder.tcp_ether(
-                self.pkt_info.src_mac, self.pkt_info.src_ip, self.pkt_info.src_port,
-                self.pkt_info.dst_mac, self.pkt_info.dst_ip, 53
-            );
-            socket.send(tcp_pkt);
+            Self::send_tcp(tools);
+            self.pkts_sent += 1;
         }
 
         if self.args.udp {
-            let udp_pkt = pkt_builder.udp_ether(
-                self.pkt_info.src_mac, self.pkt_info.src_ip, self.pkt_info.src_port,
-                self.pkt_info.dst_mac, self.pkt_info.dst_ip, 80
-            );
-            socket.send(udp_pkt);
+            Self::send_udp(tools);
+            self.pkts_sent += 1;
         }
         
         if self.args.icmp {
-            let icmp_pkt = pkt_builder.icmp_ping_ether(
-                self.pkt_info.src_mac, self.pkt_info.src_ip,
-                self.pkt_info.dst_mac, self.pkt_info.dst_ip
-            );
-            socket.send(icmp_pkt);
+            Self::send_icmp(tools);
+            self.pkts_sent += 1;
         }
     }
 
-    
 
-    fn display_progress(&mut self) {
-        self.pkts_sent += 2;
-        let msg: String = format!("Packets sent: {}", &self.pkts_sent);
-        inline_display(msg);
+
+    #[inline]
+    fn send_tcp(tools: &mut PktTools) {
+        let tcp_pkt = tools.builder.tcp_ether(
+            tools.info.src_mac, tools.info.src_ip, tools.info.src_port,
+            tools.info.dst_mac, tools.info.dst_ip, 53
+        );
+        tools.socket.send(tcp_pkt);
     }
 
+
+
+    #[inline]
+    fn send_udp(tools: &mut PktTools) {
+        let udp_pkt = tools.builder.udp_ether(
+            tools.info.src_mac, tools.info.src_ip, tools.info.src_port,
+            tools.info.dst_mac, tools.info.dst_ip, 80
+        );
+        tools.socket.send(udp_pkt);
+    }
+
+
+
+    #[inline]
+    fn send_icmp(tools: &mut PktTools) {
+        let icmp_pkt = tools.builder.icmp_ping_ether(
+            tools.info.src_mac, tools.info.src_ip,
+            tools.info.dst_mac, tools.info.dst_ip
+        );
+        tools.socket.send(icmp_pkt);
+    }
+
+}
+
+
+
+struct PktTools {
+    info:    PacketInfo,
+    builder: PacketBuilder,
+    socket:  Layer2RawSocket
+}
+
+
+
+struct PacketInfo {
+    src_mac:  [u8; 6],
+    src_ip:   Ipv4Addr,
+    src_port: u16,
+    dst_mac:  [u8; 6],
+    dst_ip:   Ipv4Addr,
+}
+
+
+impl PacketInfo {
+    fn new() -> Self {
+        Self {
+            src_mac:  [0; 6],
+            src_ip:   Ipv4Addr::new(0, 0, 0, 0),
+            src_port: 0,
+            dst_mac:  [0; 6],
+            dst_ip:   Ipv4Addr::new(0, 0, 0, 0)
+        }
+    }
 }
