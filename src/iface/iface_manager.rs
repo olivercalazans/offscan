ed8aea41e3a023a18df264baf6e9bfa28d2d224d
Oliver Calazans <oliver.calazans@gmail.com>
2026-01-03 13:23:44 -0300

    refac wifi mapper display

--- src/iface/iface_manager.rs
+++ src/iface/iface_manager.rs
@@ -1,4 +1,5 @@
 use std::ffi::CString;
+use crate::utils::abort;
 
 
 
@@ -12,12 +13,12 @@ pub struct IfaceManager;
 
 impl IfaceManager {
 
-    pub fn set_channel(iface: &str, channel: u32) -> bool {
-        let c_iface   = CString::new(iface).expect("CString::new failed");
-        let c_channel = channel as i32;
+    pub fn set_channel(iface: &str, channel: i32) -> bool {
+        let c_iface   = CString::new(iface)
+            .unwrap_or_else(|e| abort(e.to_string()));
 
         unsafe {
-            let result = set_wifi_channel(c_iface.as_ptr(), c_channel);
+            let result = set_wifi_channel(c_iface.as_ptr(), channel);
             if result != 0 {
                 return false;
             }
