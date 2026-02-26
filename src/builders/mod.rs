873dd8b6fff50a3c94e4abe2e57d2e72111ab11f
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2026-02-26 17:48:38 -0300

    Delete src directory

--- src/builders/mod.rs
+++
@@ -1,5 +0,0 @@
-pub(crate) mod ieee80211;
-pub(crate) use ieee80211::{DeauthFrame, Beacon};
-
-pub(crate) mod packets;
-pub(crate) use packets::{IcmpPkt, TcpPkt, UdpPkt, UdpPayloads};
--- src/sockets/mod.rs
+++
@@ -1,5 +0,0 @@
-pub(crate) mod l2_socket;
-pub(crate) use l2_socket::Layer2Socket;
-
-pub(crate) mod l3_socket;
-pub(crate) use l3_socket::Layer3Socket;
