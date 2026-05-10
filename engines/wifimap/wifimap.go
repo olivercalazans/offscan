a8823beaa86eb517d0fa0cb9169b476c083aaab8
Oliver Calazans <oliver.calazans@gmail.com>
2026-05-10 12:04:25 -0300

    add functions to return channels

--- engines/wifimap/wifimap.go
+++ engines/wifimap/wifimap.go
@@ -173,17 +173,14 @@ func (wm *wifiMapper) addInfo(
 
 
 func (wm *wifiMapper) sniff2GChannels() {
-    channels := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
+    channels := ifconfig.Channels2()
     wm.sniffChannels(channels, "2.4")
 }
 
 
 
 func (wm *wifiMapper) sniff5GChannels() {
-    channels := []int{
-        36,  40,  44,  48,  52,  56,  60,  64,  100, 104, 108, 112, 116, 120, 
-        124, 128, 132, 136, 140, 144, 149, 153, 157, 161, 165,
-    }
+    channels := ifconfig.Channels5()
     wm.sniffChannels(channels, "5")
 }
 
